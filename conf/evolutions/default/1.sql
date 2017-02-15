0df8a047592d5613e6b1afded7ce5ba18abf1c97
ioaccept <frey1@hm.edu>
2017-02-15 19:55:28 +0100

    Deploy

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,13 +1,12 @@
 # Users schema
- 
 # --- !Ups
- 
-CREATE TABLE Users (
-    id serial PRIMARY KEY,,,,,
-    name varchar(255) NOT NULL UNIQUE,
-    password varchar(255),
-    mentor boolean
+CREATE TABLE "Users" (
+    "id" serial PRIMARY KEY,
+    "name" varchar(255) NOT NULL UNIQUE,
+    "password" varchar(255),
+    "mentor" boolean
 );
 
-INSERT INTO Users values (1, 'ioaccept', '123', true);
+# --- !Downs
 
+drop table "USERS";
